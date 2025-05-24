5c82ffbdcd397a06da950b30ed1a6d183c30e271
Christian Vieyra <116327577+Vieyron@users.noreply.github.com>
2025-05-24 03:10:50 -0600

    Se establecio en los settings de donde venia el DIRS en los templates. Se importo el url con home. Se creo una clase View.py en el cual le pusimos el index. Se creo el index que se muestra en la pantalla

--- core/settings.py
+++ core/settings.py
@@ -1,5 +1,6 @@
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -10,7 +11,7 @@ SECRET_KEY = 'django-insecure-88-4^ue6hf+_g*@wbtm7*!c11b^+@o8!$vst7&%^lmdku8pfh@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -22,6 +23,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'core',
 ]
 
 MIDDLEWARE = [
@@ -39,7 +42,7 @@ ROOT_URLCONF = 'core.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
